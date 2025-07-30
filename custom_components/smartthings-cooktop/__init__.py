b7eedb0f0bce8747ae39aff71a1abba7caa942f3
dzmitry.dydyshka <dzmitry.dydyshka@intradiem.com>
2025-07-30 22:58:58 +0200

    Reafctoring of all API clients
    - Refactores  Smart Things API client
    - Adds more logging
    
    Change-Id: I0f8bb40e73e3f250dd1ec40dcef7455e64913999

--- custom_components/smartthings-cooktop/__init__.py
+++ custom_components/smartthings-cooktop/__init__.py
@@ -1,6 +1,7 @@
 """The samsung_cooktop integration."""
-
 from __future__ import annotations
+
+import logging
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
@@ -10,17 +11,25 @@ from .smart_things_api import CooktopAPI
 from .oauth_smart_thing import OauthSessionContext
 from .coordinator import CooktopDataUpdateCoordinator
 
+_LOGGER = logging.getLogger(__name__)
+
+def cooktop_api_init() -> CooktopAPI:
+    oauth_context = OauthSessionContext()
+    cooktop_api = CooktopAPI(oauth_context.get_session())
+    return cooktop_api
 
 async def async_setup(hass: HomeAssistant, config: dict):
     return True
 
-
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    oauth_context = OauthSessionContext()
-    api = CooktopAPI(oauth_context.get_session())
-
+    _LOGGER.info("Setup entry flow. Very begnning")
+    
+    cooktop_api = cooktop_api_init()
     device_id = entry.data["device_id"]
-    coordinator = CooktopDataUpdateCoordinator(hass, api, device_id,
+
+    _LOGGER.info(f"The device with id {device_id} will be mointored by the integration. Setting up the rest")
+
+    coordinator = CooktopDataUpdateCoordinator(hass, cooktop_api, device_id,
                                                 ["burner-01", "burner-02", "burner-03", "burner-04"])
 
     await coordinator.async_config_entry_first_refresh()
--- custom_components/smartthings-cooktop/coordinator.py
+++ custom_components/smartthings-cooktop/coordinator.py
@@ -1,21 +1,26 @@
 from datetime import timedelta
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+from .smart_things_api import CooktopAPI
 import logging
 
 _LOGGER = logging.getLogger(__name__)
 
 class CooktopDataUpdateCoordinator(DataUpdateCoordinator):
-    def __init__(self, hass, api, device_id, burner_ids):
+    def __init__(self, hass, cooktop_api: CooktopAPI, device_id, burner_ids):
         super().__init__(
             hass,
             _LOGGER,
             name="Cooktop Coordinator",
             update_interval=timedelta(seconds=7),
         )
-        self.api = api
-        self.device_id = device_id
-        self.burner_ids = burner_ids
+        self._cooktop_api = cooktop_api
+        self._device_id = device_id
+        self._burner_ids = burner_ids
+
+    @property
+    def burner_ids(self):
+        return self._burner_ids
 
     async def _async_update_data(self):
         _LOGGER.info("Running the job to retrieve the cooktop details")
-        return await self.hass.async_add_executor_job(self.api.get_cooktop_burners_status, self.device_id, self.burner_ids)
+        return await self.hass.async_add_executor_job(self._cooktop_api.get_cooktop_burners_status, self._device_id, self._burner_ids)
